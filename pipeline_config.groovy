e89a7996935d4277fcf1df09e88bf8e648c207f6
Chris Scheller <schelcj@pobox.com>
2022-06-25 09:32:42 -0400

    set the library config values

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -11,5 +11,8 @@ libraries {
   perl
   mkdocs {
     bb_repo_slug = 'config-structured'
+    bb_dest_branch = 'master'
+    bb_workspace_name = 'concertpharmaceuticals'
+    bb_api_base_url = 'https://api.bitbucket.org/2.0/repositories/'
   }
 }
